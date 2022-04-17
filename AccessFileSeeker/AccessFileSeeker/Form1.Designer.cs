7153c7396e5c5d3ce2de93655703a42f9d6f30ef
Kubi5 <Kubi5@users.noreply.github.com>
2022-04-17 22:23:10 +0200

    troszke pracy wlozylem

--- AccessFileSeeker/AccessFileSeeker/Form1.Designer.cs
+++ AccessFileSeeker/AccessFileSeeker/Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -50,30 +50,22 @@
             // label2
             // 
             this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label2.Location = new System.Drawing.Point(294, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(426, 41);
             this.label2.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(41, 178);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(206, 357);
-            this.label3.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(41, 137);
+            this.label4.Location = new System.Drawing.Point(41, 162);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(206, 41);
+            this.label4.Size = new System.Drawing.Size(206, 28);
             this.label4.TabIndex = 3;
             this.label4.Text = "Tables: ";
             // 
@@ -98,16 +90,26 @@
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(45, 193);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 338);
+            this.listBox1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(923, 682);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -120,11 +122,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- AccessFileSeeker/AccessFileSeeker/Form1.cs
+++ AccessFileSeeker/AccessFileSeeker/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace AccessFileSeeker
 {
     public partial class Form1 : Form
     {
+        OleDbConnection dbConnection;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,12 +23,44 @@ namespace AccessFileSeeker
         private void button1_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = new DialogResult();
+            string sConStr;
 
             saveFileDialog1.Title = "Select a Database File";
-            saveFileDialog1.Filter = "MDB Files (*.mdb) | (*.mdb)";
+            saveFileDialog1.Filter = "MDB files|*.mdb";
             saveFileDialog1.CheckFileExists = true;
 
             dialogResult = saveFileDialog1.ShowDialog();
+
+            if (dialogResult != DialogResult.Cancel && saveFileDialog1.FileName.Length > 0)
+            {
+                sConStr = "Provider=Microsoft.Jet.OLEDB.4.0;";
+                sConStr += "Data Source=" + saveFileDialog1.FileName + ";";
+
+
+                if(dbConnection != null)
+                {
+                    dbConnection.Close();
+                    dbConnection = null;
+                }
+
+                dbConnection = new OleDbConnection(sConStr);
+                dbConnection.Open();
+
+                string[] filename_path = saveFileDialog1.FileName.ToString().Split((char)92);
+                label2.Text = filename_path[filename_path.Length-1];
+
+                DataTable schemaTable = dbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables,
+                    new Object[]{null,null,null,"Table"});
+
+                dbConnection.Close();
+
+                listBox1.Items.Clear();
+
+                for(int i = 0; i < schemaTable.Rows.Count; i++)
+                {
+                    listBox1.Items.Add(schemaTable.Rows[i].ItemArray[2].ToString());
+                }
+            }
         }
     }
 }
