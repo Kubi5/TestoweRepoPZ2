b93fcc582bf7f2b0a602ebd9dfd045299c77ba26
Kubi5 <Kubi5@users.noreply.github.com>
2022-04-26 23:29:22 +0200

    do 6 zadania nawet ogarniete

--- EntityFrameworkDB/EntityFrameworkDB/Program.cs
+++ EntityFrameworkDB/EntityFrameworkDB/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,13 @@ namespace EntityFrameworkDB
     {
         static void Main(string[] args)
         {
-           SoftUniEntities softUniEntities = new SoftUniEntities();
-            using (softUniEntities)
+            SoftUniEntities context = new SoftUniEntities();
+            using (context)
             {
 
                 //ZADANIE 1
                 /*
-                foreach (var c in softUniEntities.Employees)
+                foreach (var c in context.Employees)
                 {
                     Console.WriteLine(c.FirstName + " " + c.LastName + " " + c.MiddleName + " " +
                         c.JobTitle + " " + Math.Round(c.Salary,2));
@@ -26,7 +27,7 @@ namespace EntityFrameworkDB
                 //ZADANIE 2
 
                 /*
-                var EmployeeNames = softUniEntities.Employees
+                var EmployeeNames = context.Employees
                     .Where(e => e.Salary > 50000)
                     .OrderBy(e => e.FirstName)
                     .Select(e => e.FirstName + " " + e.Salary);
@@ -39,15 +40,140 @@ namespace EntityFrameworkDB
 
                 //ZADANIE 3
 
-                var selectedEmployees = softUniEntities.Employees
-                    .Select(e => e.FirstName + " " + e.LastName + " " + e.Departments.)
+                /*
+                var selectedEmployees = from e in context.Employees
+                                        join d in context.Departments
+                                        on e.DepartmentID equals d.DepartmentID
+                                        where d.Name == "Research and Development"
+                                        orderby e.Salary ascending, e.FirstName descending
+                                        select e.FirstName + " " + e.LastName + " from " + d.Name + 
+                                        " - " + Math.Round(e.Salary,2);
 
+                foreach (var employee in selectedEmployees)
+                {
+                    Console.WriteLine(employee);
+                }
                 
+                */
+
+                //ZADANIE 4
+
+                /*
+                Addresses newAddress = new Addresses
+                {
+                    AddressID = 501,
+                    AddressText = "Warszawa, Krakowska 41"
+                };
+
+                Console.WriteLine(context.Addresses.Count().ToString());
+                context.Addresses.Add(newAddress);
+                context.SaveChanges();
+                Console.WriteLine(context.Addresses.Count().ToString());
+                */
+                /*
+                
+                //biore id pracownika do zmiany
+                var NakovID = from emp in context.Employees
+                              where emp.LastName == "Nakov"
+                              select emp.EmployeeID;
+                
+                //wsrod tych id modyfikuje dane pole w innej tabeli
+                foreach (var itemID in NakovID)
+                {
+                    var EmployeetoUpdate = context.Employees
+                    .Include("Addresses")
+                    .Where(x => x.EmployeeID.Equals(itemID))
+                    .First();
+                        EmployeetoUpdate.Addresses.AddressText = "Góralska 43, Chocznia";
+                }
+
+                //zapisuje zmiany
+                context.SaveChanges();
+               
+
+                //sprawdzam czy dziala
+                var employees = from emp in context.Employees
+                                join adr in context.Addresses
+                                on emp.AddressID equals adr.AddressID
+                                where emp.LastName == "Nakov"
+                                select emp.LastName + " " + adr.AddressText + " " + adr.TownID;
+                   
+                foreach (var employee in employees)
+                {
+                     Console.WriteLine(employee);
+                }
+                */
 
 
+                //ZADANIE 5
+                /*
+                int counter = 0;
+                DateTime d2001 = new DateTime(2001, 1, 1, 0, 0, 0);
+                DateTime d2003 = new DateTime(2003, 12, 31, 23, 59, 59);
+               
+                
+                foreach(var emp in context.Employees)
+                {
+                    if (emp.Projects.Count > 0)
+                    {
+                        
+                        Console.WriteLine(emp.FirstName + " " + emp.LastName +
+                            " - Menager: " + emp.Employees2.FirstName + " " + emp.Employees2.LastName);
+
+                        var projects = from pro in emp.Projects
+                                       where pro.StartDate >= d2001 && pro.StartDate <= d2003
+                                       select pro;
+
+                        foreach (var project in projects)
+                        {
+                            if (project.EndDate == null)
+                            {
+                                Console.WriteLine("-- " + project.Name + " - "
+                                    + project.StartDate + " - not finished");
+                            }
+                            else
+                            {
+                                Console.WriteLine("-- " + project.Name + " - "
+                                    + project.StartDate + " - " + project.EndDate);
+                            }
+                        }
+                    }
+                    counter++;
+                    if(counter == 30)
+                    {
+                        break;
+                    }
+                }
+                    */
+
+                //ZADANIE 6
+                /*
+                var addresses = from adr in context.Addresses
+                                join emp in context.Employees
+                                on adr.AddressID equals emp.AddressID
+                                group new { adr, emp } by new { adr.AddressText } into g
+                                let r = g.FirstOrDefault()
+                                orderby g.Count() descending
+                                select new
+                                {
+                                    addressText = r.adr.AddressText,
+                                    count = g.Count()
+                                    
+                                };
+                       
+                foreach(var address in addresses.Take(10))
+                {
+                    Console.WriteLine(address.addressText + " - " + address.count + " employee/s");
+                }
+                    */
+                
+                //ZADANIE 7
 
 
-             }
+            
+
+
+            }
         }
     }
 }
